51bd5b53bc1e3268919779dc1e4e787186311af2
jaesekim <jaeseki@gmail.com>
2024-06-05 16:59:31 +0900

    :tada: Start: 프로젝트 시작

---
+++ inspire/inspire/inspireApp.swift
@@ -0,0 +1,17 @@
+//
+//  inspireApp.swift
+//  inspire
+//
+//  Created by 김재석 on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct inspireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
